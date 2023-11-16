eddf31de4c97bdb29ec1327d4bf26ec3d82bf3f4
RandomSkill <simon.sabot@Gmail.com>
2023-11-16 22:52:08 +0100

    add: prettier and EslInt config file

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  jsxSingleQuote: false,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  trailingComma: 'all',
+  embeddedLanguageFormatting: 'auto',
+};
