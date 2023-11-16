c1b1163002cd04f2509d0792fed5c73b99f3f680
ali beztout <ali.beztout@student.vinci.be>
2023-11-16 19:53:16 +0100

    add case 4 + refactoring and tests

--- greet.js
+++ greet.js
@@ -1,14 +1,20 @@
 
-function greet(name) {
-    if (!name || name.trim() === '') {
+function greet(names) {
+    if (!names || (Array.isArray(names) && names.length === 0) || (typeof names === 'string' && names.trim() === '')) {
         return "Hello, my friend.";
     }
 
-    if (name === name.toUpperCase()) {
-        return "HELLO, " + name + "!";
+    if (Array.isArray(names)) {
+        if (names.length === 2) {
+            return "Hello, " + names[0] + " and " + names[1] + ".";
+        }
     }
 
-    return "Hello, " + name + ".";
+    if (names === names.toUpperCase()) {
+        return "HELLO, " + names + "!";
+    }
+
+    return "Hello, " + names + ".";
 }
 
 
